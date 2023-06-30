83adb5de83297da74d85b7e84d96987a7b6d6215
JesusDBS <jesusbingucv@gmail.com>
2023-06-29 20:46:17 -0400

    [ADD] AddBookmarkCommad

--- commands.py
+++ commands.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from database import DatabaseManager
 
 db = DatabaseManager(db_filename='bookmark.db')
+
 
 class CreateBookmarksTableCommand:
     """This class creates the bookmarks table
@@ -24,4 +26,30 @@ class CreateBookmarksTableCommand:
         db.create_table(
             table_name=table_name,
             columns=columns
+        )
+
+
+class AddBookmarkCommand:
+    """This class adds new bookmarks into the database
+    """
+    date_added = datetime.utcnow().isoformat()
+    table_name = 'bookmarks'
+
+    @classmethod
+    def execute(cls, data: dict) -> str:
+        """Creates bookmarks records
+        """
+        assert isinstance(data, dict), f'data: {data} must be a dictionary!'
+
+        if 'date_added' in data.keys():
+            data['date_added'] = cls.date_added
+
+        else:
+            data.update(
+                {'date_added': cls.date_added}
+            )
+
+        db.create_record(
+            table_name=cls.table_name,
+            columns_values=data
         )
