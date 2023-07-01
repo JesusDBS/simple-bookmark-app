95e3a06141cdd8678cf2dbc6303055f9270567a9
JesusDBS <jesusbingucv@gmail.com>
2023-07-01 10:59:35 -0400

    [ADD] Option class

--- app.py
+++ app.py
@@ -1,4 +1,50 @@
+from typing import Any
 import commands
+
+
+class Options:
+    """These class holds the logic to be executed when a user choose an option.
+    """
+
+    def __init__(self, name_to_display: str, command, preparation_step=None) -> None:
+        self.name_to_display = name_to_display
+        self.command = command
+        self.preparation_step = preparation_step
+
+    # def get_preparation_step_data(self):
+    #     """Takes data from user
+    #     """
+    #     keys = ['title', 'url', 'notes', 'date_added']
+
+    #     message = """Please provide the info in the following order:
+    #         1) bookmark's title
+    #         2) bookmark's URL
+    #         3) bookmark's notes
+    #         4) bookmark's creation date\n
+    #     """
+    #     print(message)
+
+    #     user_input = input(
+    #         "Please give the bookmark info separated by one space: ")
+
+    #     values = [value.strip() for value in user_input.split(' ') if value]
+
+    #     self.data = dict(zip(keys, values))
+
+    def __call__(self):
+        data = self.preparation_step() if self.preparation_step else None
+
+        if data:
+            message = self.command.execute(data)
+
+        else:
+            message = self.command.execute()
+
+        print(message)
+
+    def __str__(self) -> str:
+        return self.name_to_display
+
 
 if __name__ == "__main__":
     commands.CreateBookmarksTableCommand().execute()
